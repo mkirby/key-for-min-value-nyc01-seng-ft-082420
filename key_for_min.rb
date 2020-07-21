467de70cf962991360a67c89d12591d12bb9a004
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 15:50:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,14 @@ def key_for_min_value(name_hash)
   min_value_key = nil
   
   name_hash.each do |key, value|
-    
+    if min_value = nil
+      min_value = value
+      min_value_key = key
+    end
+    if value < min_value
+      min_value = value
+      min_value_key = key
+    end
   end
-
+  min_value_key
 end
