09e9b12c2715d590c5bf6d34468d2be7692d09f1
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 15:52:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,10 +10,6 @@ def key_for_min_value(name_hash)
       min_value = value
       min_value_key = key
     end
-    if value < min_value
-      min_value = value
-      min_value_key = key
-    end
   end
   min_value_key
 end
